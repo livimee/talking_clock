e9d6da2b440ba3a3bd579db6df8ad3e5a0bb8728
livimee <oliviastarr26@gmail.com>
2017-07-18 21:36:41 -0400

    Update talkingclock.py
    added comments

--- talkingclock.py
+++ talkingclock.py
@@ -12,11 +12,12 @@ def time_to_text(time):
 	hour = int(time[0])
 	minute = int(time[1])
 
+	# Determines a.m. or p.m.
 	meridiem = "a.m."
-
 	if hour >= 12:
 		meridiem = "p.m."
 
+	# Converts hour to words
 	if hour == 0 or hour == 12:
 		hour_text = "twelve "
 	elif hour > 12:
@@ -25,6 +26,7 @@ def time_to_text(time):
 	else:
 		hour_text = p.number_to_words(hour) + " "
 
+	# Converts minutes to words
 	if minute == 0:
 		minute_text = ""
 	elif minute < 10:
@@ -32,14 +34,18 @@ def time_to_text(time):
 	else:
 		minute_text = p.number_to_words(minute) + " "
 
+	# Combines and returns full time statement
 	time_text = "It is" + hour_text + minute_text + meridiem
-
 	return time_text
 
+# Accesses and formats current time
 time = str(datetime.now())
 time = time.split(" ")
 time = time[1]
 time = time.split(":")
 time = str(time[0]) + ":" + str(time[1])
 
+# Calls os speech function to recite given time.
+# the time is converted from numbers to words using
+# the time_to_text() function.
 subprocess.call(["say", time_to_text(time)])
