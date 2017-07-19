e9459f929eab01384be985dc90399b09cffdd126
livimee <oliviastarr26@gmail.com>
2017-07-18 21:19:03 -0400

    Update talkingclock.py
    updated

--- talkingclock.py
+++ talkingclock.py
@@ -1,25 +1,22 @@
 # Talking Clock
-# (r/dailyprogrammer challenge)
-#
-# Converts a given time in the format "##:##" from numbers to words.
 
 import inflect
+import subprocess
+from datetime import datetime
+
 p = inflect.engine()
 
-# Completes and displays the conversion
 def time_to_text(time):
 	time = time.split(":")
 
-	# Converts given time to integers
 	hour = int(time[0])
 	minute = int(time[1])
 
-	# Determines a.m. or p.m.
-	meridiem = "a.m"
+	meridiem = "a.m."
+
 	if hour >= 12:
-		meridiem = "p.m"
-	
-	# Converts Hours
+		meridiem = "p.m."
+
 	if hour == 0 or hour == 12:
 		hour_text = "twelve "
 	elif hour > 12:
@@ -28,22 +25,21 @@ def time_to_text(time):
 	else:
 		hour_text = p.number_to_words(hour) + " "
 
-	# Converts minutes
 	if minute == 0:
 		minute_text = ""
 	elif minute < 10:
 		minute_text = "oh " + p.number_to_words(minute) + " "
 	else:
 		minute_text = p.number_to_words(minute) + " "
-	
-	# Concatonates full time statement
-	time_text = "It's " + hour_text + minute_text + meridiem
 
-	print(time_text)
+	time_text = "It is" + hour_text + minute_text + meridiem
 
-	
-# Time to be converted is accepted via user input
-time = raw_input("Enter time: ")
+	return time_text
 
-time_to_text(time)
+time = str(datetime.now())
+time = time.split(" ")
+time = time[1]
+time = time.split(":")
+time = str(time[0]) + ":" + str(time[1])
 
+subprocess.call(["say", time_to_text(time)])
